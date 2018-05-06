7381fe9a8f9eaf121d41fa504f3566532ffc041a
danielheyman <daniel.heyman@gmail.com>
2018-05-06 00:09:04 -0400

    Fix neotracker

--- src/services/neotracker.js
+++ src/services/neotracker.js
@@ -4,8 +4,8 @@ const req = Bluebird.promisify(require("request"));
 const W3CWebSocket = require('websocket').w3cwebsocket;
 
 const hashMapper = {
-    "602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7": "GAS",
-    "c56f33fc6ecfcd0c225c4ab356fee59390af8560be0e930faebe74a6daff7c9b": "NEO",
+    "Asset:602c79718b16e442de58778e148d0b1084e3b2dffd5de6b7b16cee7969282de7": "GAS",
+    "Asset:c56f33fc6ecfcd0c225c4ab356fee59390af8560be0e930faebe74a6daff7c9b": "NEO",
 };
 
 module.exports = {
@@ -22,8 +22,8 @@ module.exports = {
             var ws = new W3CWebSocket('wss://neotracker.io/graphql', "graphql-ws");
             
             const sendMessage = (() => {
-                ws.send(JSON.stringify({"type":"GQL_START","id":"2","query":{"id":"33","variables":{"hash":addr}}}));
-                ws.send(JSON.stringify({"type":"GQL_START","id":"2","query":{"id":"33","variables":{"hash":addr}}}));
+                ws.send(JSON.stringify({"type":"GQL_START","id":"2","query":{"id":"16","variables":{"hash":addr}}}));
+                ws.send(JSON.stringify({"type":"GQL_START","id":"2","query":{"id":"16","variables":{"hash":addr}}}));
             });
 
             ws.onopen = sendMessage;
@@ -36,7 +36,7 @@ module.exports = {
                     }
                     const mapped = data.value.data.address.coins.edges.map(coin => {
                         return {
-                            asset: hashMapper[coin.node.asset.hash] || coin.node.asset.symbol,
+                            asset: hashMapper[coin.node.asset.id] || coin.node.asset.symbol,
                             quantity: parseFloat(coin.node.value)
                         };
                     })
