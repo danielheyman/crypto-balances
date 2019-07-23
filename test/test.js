c2a9e9644b77dce7e7be9fac62c7abd58b349df1
danielheyman <daniel.heyman@gmail.com>
2019-07-23 19:01:19 -0400

    update packages

--- test/test.js
+++ test/test.js
@@ -7,104 +7,92 @@ const balance = require("../src/crypto-balance");
 
 describe("Balance", function() {
     // blockchain-info
-    it("has an xpub balance", done =>
+    it("has an xpub balance", () =>
         balance("xpub6CUGRUonZSQ4TWtTMmzXdrXDtypWKiKrhko4egpiMZbpiaQL2jkwSB1icqYh2cfDfVxdx4df189oLKnC5fSwqPfgyP3hooxujYzAu3fDVmz").then((result) => {
             expect(result.address_type).to.equal("BTC");
             expect(result.balances.BTC).to.exist;
-            return done();
         })
     );
 
     // neoscan
-    it("has a NEO & GAS balance", done =>
+    it("has a NEO & GAS balance", () =>
         balance("AKDVzYGLczmykdtRaejgvWeZrvdkVEvQ1X").then((result) => {
             expect(result.address_type).to.equal("NEO");
             expect(result.balances.NEO).to.exist;
             expect(result.balances.GAS).to.exist;
-            return done();
         })
     );
 
     // chain-so
-    it("has a BTC balance", done =>
+    it("has a BTC balance", () =>
         balance("3D2oetdNuZUqQHPJmcMDDHYoqkyNVsFk9r").then((result) => {
             expect(result.address_type).to.equal("BTC");
             expect(result.balances.BTC).to.exist;
-            return done();
         })
     );
     
-    it("has an LTC balance", done =>
+    it("has an LTC balance", () =>
         balance("LdP8Qox1VAhCzLJNqrr74YovaWYyNBUWvL").then((result) => {
             expect(result.address_type).to.equal("LTC");
             expect(result.balances.LTC).to.exist;
-            return done();
         })
     );
     
-    it("has a DASH balance", done =>
+    it("has a DASH balance", () =>
         balance("XekiLaxnqpFb2m4NQAEcsKutZcZgcyfo6W").then((result) => {
             expect(result.address_type).to.equal("DASH");
             expect(result.balances.DASH).to.exist;
-            return done();
         })
     );
     
-    it("has a DOGE balance", done =>
+    it("has a DOGE balance", () =>
         balance("D8EyEfuNsfQ3root9R3ac54mMcLmoNBW6q").then((result) => {
             expect(result.address_type).to.equal("DOGE");
             expect(result.balances.DOGE).to.exist;
-            return done();
         })
     );
 
     // ethplorer
-    it("has an ETH & ERC20 balance", done =>
+    it("has an ETH & ERC20 balance", () =>
         balance("0x1ebacb7844fdc322f805904fbf1962802db1537c").then((result) => {
             expect(result.address_type).to.equal("ETH");
             expect(result.balances.ETH && result.balances.OMG).to.exist;
-            return done();
         })
     );
 
     // Chainz
-    it("has a LTC Sewgit balance", done =>
+    it("has a LTC Sewgit balance", () =>
         balance("MJejLHNVJLdkp9RM97AyFE85qmpPzh8PYw").then((result) => {
             expect(result.address_type).to.equal("LTC");
             expect(result.balances.LTC).to.exist;
-            return done();
         })
     );
 
-    it("has a Stratus balance", done =>
+    it("has a Stratus balance", () =>
         balance("SN7bBW1RRpu3PsSwcKWF74ZDL9UNG32edd").then((result) => {
             expect(result.address_type).to.equal("STRAT");
             expect(result.balances.STRAT).to.exist;
-            return done();
         })
     );
 
-    it("has a DigiByte balance", done =>
+    it("has a DigiByte balance", () =>
         balance("DCo1dbnnwWB4cucwSduXMdTV1tDErZHNfx", "DGB").then((result) => {
             expect(result.address_type).to.equal("DGB");
             expect(result.balances.DGB).to.exist;
-            return done();
         })
     );
 
     // specific coins
-    it("supports a specific coin type - invalid", done => 
+    it("supports a specific coin type - invalid", () => 
         balance("0x1ebacb7844fdc322f805904fbf1962802db1537c", "BTC").then(result => {
             expect(result.address_type).to.equal("unknown");
-            return done();
         })
     );
 
-    it("supports a specific coin type - valid", done =>
+    it("supports a specific coin type - valid", () =>
         balance("0x1ebacb7844fdc322f805904fbf1962802db1537c", "ETH").then((result) => {
             expect(result.address_type).to.equal("ETH");
             expect(result.balances.ETH && result.balances.OMG).to.exist;
-            return done();
         })
     );
 });
